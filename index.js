6d43ccb111ca94b0d13b7cf492b97b5944389ae0
lsamano <leizl.samano@gmail.com>
2020-02-06 18:22:17 -0500

    functional Tetris

--- index.js
+++ index.js
@@ -3,16 +3,28 @@ const context = canvas.getContext('2d') // Not sure what this does
 
 context.scale(20, 20); // make bigger
 
- const matrix = [
-   [0, 0, 0],
-   [1, 1, 1],
-   [0, 1, 0]
- ] // T piece
+function arenaSweep()
+  let rowCount = 1;
+  outer: for (let y = arena.length - 1; y > 0; --y) {
+    for (let x = 0; x < arena[y].length; ++x) {
+      if (arena[y][x] === 0) {
+          continue outer;
+      }
+    }
+
+    const row = arena.splice(y, 1)[0].fill(0)
+    arena.unshift(row);
+    ++y
+
+    player.score += rowCount * 10;
+    rowCount *= 2
+  }
+}
 
 function collide(arena, player) {
   const [m, o] = [player.matrix, player.pos]
-  for (let y = 0; y < m.length; y++) {
-    for (let x = 0; x < m[y].length; x++) {
+  for (let y = 0; y < m.length; ++y) {
+    for (let x = 0; x < m[y].length; ++x) {
       if (m[y][x] !== 0 &&
         (arena[y + o.y] &&
           arena[y + o.y][x + o.x]) !== 0) {
@@ -31,8 +43,54 @@ function createMatrix(w, h) {
   return matrix
 }
 
+function createPiece(type) {
+  if (type === 'T') {
+    return [
+      [0, 0, 0],
+      [1, 1, 1],
+      [0, 1, 0]
+    ]
+  } else if (type === 'O') {
+    return [
+      [2, 2],
+      [2, 2]
+    ]
+  } else if (type === 'L') {
+    return [
+      [0, 3, 0],
+      [0, 3, 0],
+      [0, 3, 3]
+    ]
+  } else if (type === 'J') {
+    return [
+      [0, 4, 0],
+      [0, 4, 0],
+      [4, 4, 0]
+    ]
+  } else if (type === 'I') {
+    return [
+      [0, 5, 0, 0],
+      [0, 5, 0, 0],
+      [0, 5, 0, 0],
+      [0, 5, 0, 0]
+    ]
+  } else if (type === 'S') {
+    return [
+      [0, 6, 6],
+      [6, 6, 0],
+      [0, 0, 0]
+    ]
+  } else if (type === 'Z') {
+    return [
+      [7, 7, 0],
+      [0, 7, 7],
+      [0, 0, 0]
+    ]
+  }
+}
+
  function draw() {
-   context.fillStyle = '#000'
+   context.fillStyle = '#514444'
    context.fillRect(0, 0, canvas.width, canvas.height)
 
    drawMatrix(arena, {x: 0, y: 0})
@@ -44,7 +102,7 @@ function drawMatrix(matrix, offset) {
   matrix.forEach((row, y) => {
     row.forEach((value, x) => {
       if (value !==0) {
-        context.fillStyle = 'purple';
+        context.fillStyle = colors[value];
         context.fillRect(
           x + offset.x,
           y + offset.y,
@@ -71,7 +129,9 @@ function playerDrop() {
   if (collide(arena, player)) {
     player.pos.y--
     merge(arena, player)
-    player.pos.y = 0
+    playerReset()
+    arenaSweep()
+    updateScore()
   }
   dropCounter = 0
 }
@@ -80,6 +140,18 @@ function playerMove(dir) {
   player.pos.x += dir
   if (collide(arena, player)) {
     player.pos.x -= dir
+  }
+}
+
+function playerReset() {
+  const pieces = 'ILJOSTZ'
+  player.matrix = createPiece(pieces[pieces.length * Math.random() | 0])
+  player.pos.y = 0;
+  player.pos.x = (arena[0].length / 2 | 0) - (player.matrix[0].length / 2 | 0)
+  if (collide(arena, player)) {
+    arena.forEach(row => row.fill(0))
+    player.score = 0
+    updateScore()
   }
 }
 
@@ -135,11 +207,22 @@ function update(time = 0) {
   requestAnimationFrame(update)
 }
 
+function updateScore() {
+  const score = document.getElementById('score')
+  score.innerText = player.score
+
+}
+
+const colors = [
+  null, 'blueviolet', 'gold', 'darkorange', 'blue', 'cyan', 'chartreuse', '#FF0032'
+]
+
 const arena = createMatrix(12, 20)
 
 const player = {
-  pos: { x: 5, y: 5 },
-  matrix: matrix
+  pos: { x: 0, y: 0 },
+  matrix: null,
+  score: 0
 }
 
 document.addEventListener('keydown', event => {
@@ -156,4 +239,6 @@ document.addEventListener('keydown', event => {
   }
 })
 
+playerReset()
+updateScore()
 update()
