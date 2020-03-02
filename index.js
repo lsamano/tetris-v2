cbb8bb153ab50bd2929c4188edb39cfc40bbbd9a
lsamano <leizl.samano@gmail.com>
2020-03-02 17:53:39 -0500

    small edits

--- index.js
+++ index.js
@@ -9,41 +9,36 @@ const playerElements = document.querySelectorAll('.full-game');
 
 const keyListener = event => {
   // [hold, up, left, right, down, rotaLeft, rotaRight],
-
   [
     [51, 87, 65, 68, 83, 49, 50],
     [190, 38, 37, 39, 40, 75, 76],
   ].forEach((key, index) => {
     const player = bothGames[index].player
-    if (event.type === 'keydown') {
-      if (event.keyCode === key[0]) {
-        player.hold()
-      } else if (event.keyCode === key[1]) {
-        player.hardDrop()
-      } else if (event.keyCode === key[2]) {
-        player.move(-1)
-      } else if (event.keyCode === key[3]) {
-        player.move(1)
-      } else if (event.keyCode === key[5]) {
-        player.rotate(-1)
-      } else if (event.keyCode === key[6]) {
-        player.rotate(1)
-      }
-    }
-    if (event.keyCode === key[4]) {
+    if (event.keyCode === key[0]) {
+      player.hold()
+    } else if (event.keyCode === key[1]) {
+      player.hardDrop()
+    } else if (event.keyCode === key[2]) {
+      player.move(-1)
+    } else if (event.keyCode === key[3]) {
+      player.move(1)
+    } else if (event.keyCode === key[5]) {
+      player.rotate(-1)
+    } else if (event.keyCode === key[6]) {
+      player.rotate(1)
+    } else if (event.keyCode === key[4]) {
       player.drop(true)
     }
   })
 }
 
 document.addEventListener('keydown', keyListener)
-// document.addEventListener('keyup', keyListener)
 
-function startGame() {
-  tetris.updateForecast()
-  tetris.updateScore()
-  tetris.update()
-}
+// function startGame() {
+//   tetris.updateForecast()
+//   tetris.updateScore()
+//   tetris.update()
+// }
 
 // const startButton = document.getElementById("start-button")
 // startButton.addEventListener('click', event => {
